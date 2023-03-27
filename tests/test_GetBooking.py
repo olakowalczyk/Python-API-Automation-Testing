4dbb766a4b9ee2d219551e23c2f482d108c6bee4
ola <olakowalczykola@gmail.com>
2023-03-27 17:20:29 +0200

    Delete assertions

--- tests/test_GetBooking.py
+++ tests/test_GetBooking.py
@@ -12,8 +12,6 @@ class GetTests:
         """
         response = HttpManager.get(Bookings.GET_BOOKING.format(Bookings.get_random_booking()))
         assert_that(response.status_code).is_equal_to(200)
-        assert_that(response.json()['firstname']).is_instance_of(str)
-        assert_that(response.json()['depositpaid']).is_instance_of(bool)
 
 
     def test_get_booking_ids(self):
@@ -22,5 +20,5 @@ class GetTests:
         """
         response = HttpManager.get(Bookings.GET_BOOKINGS)
         assert_that(response.status_code).is_equal_to(200)
-        assert_that(response.json()[0]['bookingid']).is_instance_of(int)
+
         
