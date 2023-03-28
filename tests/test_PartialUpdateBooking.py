fa629fe814368470fb2b3025a9b7b8123ad8a7e7
ola <olakowalczykola@gmail.com>
2023-03-28 17:00:59 +0200

    Add test data for partial update

--- tests/test_PartialUpdateBooking.py
+++ tests/test_PartialUpdateBooking.py
@@ -6,21 +6,20 @@ from utils.http_manager import HttpManager
 
 
 @fixture()
-def json_for_patch_booking():
-    return json.dumps({
-        "firstname": "Edited",
-        "lastname": "Edited"
-    })
+def test_data() -> str:
+    with open('test_data/partial_update_booking.json') as data_file:
+        data = data_file.read()
+        return data
 
 
 class PatchTests:
 
-    def test_partial_update_booking(self, existing_booking_url, json_for_patch_booking):
+    def test_partial_update_booking(self, existing_booking_url, test_data):
         """
         Checks whether partial update properly updates booking data
         """
-        response = HttpManager.patch(existing_booking_url, data=json_for_patch_booking)
+        response = HttpManager.patch(existing_booking_url, data=test_data)
         assert_that(response.status_code).is_equal_to(200)
-        assert_that(response.json()['firstname']).is_equal_to(json.loads(json_for_patch_booking)['firstname'])
-        assert_that(response.json()['lastname']).is_equal_to(json.loads(json_for_patch_booking)['lastname'])
+        assert_that(response.json()['firstname']).is_equal_to(json.loads(test_data)['firstname'])
+        assert_that(response.json()['lastname']).is_equal_to(json.loads(test_data)['lastname'])
    
